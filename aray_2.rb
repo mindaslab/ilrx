5b9999cf9873c36a2f7e4f0f71c15dab3b1d111e
Karthikeyan A K <mindaslab@gmail.com>
2012-08-13 19:21:10 +0530

    some regexp examples corrected

---
+++ aray_2.rb
@@ -0,0 +1,10 @@
+# array_2.rb
+
+my_array = Array.new
+my_array << "Something"
+my_array << 123
+my_array << Time.now
+
+for element in my_array
+	puts element
+end
--- regexp_character_classes_1.rb
+++ regexp_character_classes_1.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/ruby
 # regexp_character_classes_1.rb
 
+print "Enter a short string: "
 string = gets.chop
 puts "The string contains character(s) from a to z"  if /[a-z]/.match string
 puts "The string contains character(s) from A to Z"  if /[A-Z]/.match string
 puts "The string contains number(s) from 0 to 9"  if /[0-9]/.match string
-puts "The string contains vovels"  if /[aeiou]/.match string
-puts "The string does not contains character(s) from a to z"  if /[^a-z]/.match string
-puts "The string does not contains character(s) from A to Z"  if /[^A-Z]/.match string
-puts "The string does not contains number(s) from 0 to 9"  if /[^0-9]/.match string
-puts "The string does not contains vovels"  if /[^aeiou]/.match string
+puts "The string contains vowels"  if /[aeiou]/.match string
+puts "The string contains character(s) other than a to z"  if /[^a-z]/.match string
+puts "The string contains character(s) other than A to Z"  if /[^A-Z]/.match string
+puts "The string contains number(s) other than 0 to 9"  if /[^0-9]/.match string
+puts "The string contains characters other than vowels"  if /[^aeiou]/.match string
