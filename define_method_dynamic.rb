5f8d43fa3eb086146ed5fb60582b6e485cf532e5
Karthikeyan A K <mindaslab@gmail.com>
2013-12-06 23:17:42 +0530

    metaprogramming examples added

---
+++ define_method_dynamic.rb
@@ -0,0 +1,15 @@
+# define_method_dynamic.rb
+
+Book_hash = {author: "Zigor", title: "I Love Ruby", page: 95}
+
+class Book
+ Book_hash.each do |key, value|
+    define_method key do
+      value
+    end     
+  end
+end
+
+b = Book.new
+puts "#{b.author} has written a book named #{b.title}"
+
