f60acff1267a0d46ebe372c9d5c3a0b4a2a5b4f7
Karthikeyan A K <karthikeyan.ak@zanec.com>
2014-11-12 10:37:56 +0530

    added array_for.rb

--- array_for.rb
+++ array_for.rb
@@ -1,10 +1,10 @@
-# array.rb
+# array_for.rb
 
 my_array = Array.new
 my_array.push("Something")
 my_array.push 123
 my_array << Time.now
 
-my_array.each do |element|
+for element in my_array
   puts element
 end
