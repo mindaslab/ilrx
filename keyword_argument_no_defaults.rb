40dae075d7bd8d1595bcd2fc006a8e2a14729b30
Karthikeyan A K <karthikeyan.ak@zanec.com>
2015-05-30 00:03:25 +0530

    key word args no defaults added

--- keyword_argument_no_defaults.rb
+++ keyword_argument_no_defaults.rb
@@ -1,9 +1,9 @@
-# keyword_argument.rb
+# keyword_argument_no_defaults.rb
 
-def say_hello name: "Martin", age: 33
+def say_hello name:, age:
   puts "Hello #{name} your age is #{age}"
 end
 
 say_hello name: "Joseph", age: 7
 say_hello age: 21, name: "Vignesh"
-say_hello
+# say_hello # uncomment it and try it out
